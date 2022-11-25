eba919f72ae5b633b1277ed762fe77dcaba883c8
bev @nun <86957585+aabbeegg@users.noreply.github.com>
2022-11-25 12:59:14 +0200

    Created the main method

---
+++ carApp.java
@@ -0,0 +1,16 @@
+package classExerise;
+
+import java.util.Scanner;
+
+public class carApp{    
+	//create the main class   
+
+	 public static void main(String[] args) {	
+Scanner in=new Scanner (System.in);
+Car car=new Car();
+Car car2=new Car();
+car.printData("Toyota", "RAV4", 40);
+
+String ans="";
+
+
