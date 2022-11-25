0fd5dd510fa9de4c3061afd111b458b01da7ac65
bev @nun <86957585+aabbeegg@users.noreply.github.com>
2022-11-25 13:03:07 +0200

    Added the while loop and if else statement

--- carApp.java
+++ carApp.java
@@ -13,4 +13,29 @@ car.printData("Toyota", "RAV4", 40);
 
 String ans="";
 
+//create the while loop and if else statement
+while
+	(!ans.equalsIgnoreCase("x"))
+	 {
+	System.out.println ("a=accelerate b=break x=exit");
+	ans=in.nextLine();
+	if(ans.equalsIgnoreCase("a")) {
+	car2.accelerate();
+	
+	}
+	else if(ans.equalsIgnoreCase("b"))
+	{
+	car.brake();
+	}
+	else if(ans.equalsIgnoreCase("x"))
+	{
+	break;
+	}
+
+	 
+	 }
+
+	}
+
+}
 
